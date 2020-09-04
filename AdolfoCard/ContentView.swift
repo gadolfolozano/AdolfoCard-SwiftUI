4c785f753646ee5a39507b96efa56035dd773dc7
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2020-09-03 21:45:29 -0300

    Initial Commit

---
+++ AdolfoCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdolfoCard
+//
+//  Created by Gustavo Lozano on 03/09/20.
+//  Copyright © 2020 gustavo.lozano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
